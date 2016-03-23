b1a6873a450d2ed381cdd1a1c0db1b265fd8c424
Walter Jabo <walter.jabo@gmail.com>
2016-03-22 21:10:38 -0500

    Primer commit

---
+++ src/main/java/com/wjb/sample/endpoints/Constants.java
@@ -0,0 +1,14 @@
+package com.wjb.sample.endpoints;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the
+ * helloworld API.
+ */
+public class Constants {
+	public static final String WEB_CLIENT_ID = "29260430809-jsbl72j99h655qjil1511f4omcqjrvqh.apps.googleusercontent.com";
+	public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+	public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+	public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
